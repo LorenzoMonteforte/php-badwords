5d525bf763d40fbbcc7d89bfbbae04e695f74a7b
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-12-15 16:30:03 +0100

    Esercizio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <?php
+        $title = "PHP";
+        $name = "Lorenzo";
+        $surname = "Monteforte";
+    ?>
+</head>
+<body>
+    <h1><?php echo $title ?></h1>
+    <p>Ciao a tutti il mio nome è <?php echo $name ." " .$surname?></p>
+</body>
+</html>
